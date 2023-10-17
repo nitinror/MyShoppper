18d703f338c0f8fee20ccaf92c2930cf2996ea4d
Nitin <nitinror26@gmail.com>
2023-10-17 22:24:51 +0530

    com1

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDBuyd6dhhw2pkEDkzNqdBYNb4x6KN_ByY",
+    authDomain: "shopper-e5b77.firebaseapp.com",
+    projectId: "shopper-e5b77",
+    storageBucket: "shopper-e5b77.appspot.com",
+    messagingSenderId: "299565150301",
+    appId: "1:299565150301:web:0aea6f4192a7412a2addb4",
+    measurementId: "G-GVR3L1VWB6"
+  };
